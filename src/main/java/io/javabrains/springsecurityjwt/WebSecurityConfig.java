5875d7342f1dc17b060ca14f016ef032ebf789cd
Jatin <jatin.shashoo@gmail.com>
2020-03-02 01:09:37 +0530

    Code refactoring

--- src/main/java/io/javabrains/springsecurityjwt/WebSecurityConfig.java
+++ src/main/java/io/javabrains/springsecurityjwt/WebSecurityConfig.java
@@ -39,7 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.csrf()
+				.disable()
 				.authorizeRequests()
 				.antMatchers("/authenticate")
 				.permitAll()
